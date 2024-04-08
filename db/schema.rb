801461bb9061dcf842fcaa5ac3e1a254a60ea104
draganb24 <nemanjabjelica45@gmail.com>
2024-04-08 19:32:27 +0200

    Product stocks

--- db/schema.rb
+++ db/schema.rb
@@ -69,7 +69,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_25_233146) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "stocks", force: :cascade do |t|
+    t.string "size"
+    t.integer "amount"
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_stocks_on_product_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "categories"
+  add_foreign_key "stocks", "products"
 end
